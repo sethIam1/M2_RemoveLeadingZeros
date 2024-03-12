624ac8e55e6435185393430e08d9fe78a89eba5f
sethIam1 <150064062+sethIam1@users.noreply.github.com>
2024-03-12 17:06:51 -0400

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'PT_CustomInvoice',
+    'SethIam_RemoveLeadingZeros',
     __DIR__
 );
